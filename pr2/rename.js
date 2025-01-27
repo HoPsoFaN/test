3381a8350d2f227403aebfa1e1f98484999db544
HoPsoFaN <kyly777123iop@gmail.com>
2025-01-27 19:28:20 +0500

    3 коммит

--- pr2/rename.js
+++ pr2/rename.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 
+/*
 fs.rename('hello.txt', 'async operation.txt', function (error) {
     if (error) {
         console.log(error)
@@ -7,3 +8,27 @@ fs.rename('hello.txt', 'async operation.txt', function (error) {
 })
 
 fs.renameSync('hello.txt', 'sync operation.txt')
+*/
+
+/*
+let fileContent
+
+fs.readFile('hello.txt', 'utf-8', (err, data) => {
+    if (err) throw (err)
+    fileContent = data
+    console.log(fileContent)
+})
+//*/
+
+///*
+let fileContent
+
+fs.appendFileSync('hello.txt', '\nКонец')
+
+fs.readFile('hello.txt', 'utf-8', (err, data) => {
+    if (err) throw (err)
+    fileContent = data
+    console.log(fileContent)
+    fs.unlinkSync('hello.txt')
+})
+//*/
